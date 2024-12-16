09c704297a6255aa5921f1551df2407c4027efe3
João Gustavo <joaogustavodossantoss@gmail.com>
2024-12-16 13:47:02 -0300

    Create Atividade1.py

---
+++ 2° Semestre/Desenvolvimento Orientado a Objetos/Atividade1.py
@@ -0,0 +1,13 @@
+def somar (a, b):
+  resultado = a + b
+  return resultado
+  
+def multiplicar (a, b):
+  resultado = a * b
+  return resultado
+
+def saudar(nome= "Visitante"):
+  print(f"Olá, {nome}! Seja bem-vindo ao nosso sistema.")
+
+def despedir(nome= "Visitante"):
+  print(f"Adeus, {nome}! Até a próxima.")
