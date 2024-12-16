05c3dffc7b542dfc07bbd7e7471d9b856a02bc5f
João Gustavo <joaogustavodossantoss@gmail.com>
2024-12-16 15:09:46 -0300

    Atualização nas classes
    Inseri todas as classes com seus métodos e atributos

--- 2° Semestre/Desenvolvimento Orientado a Objetos/AtividadePython3.py
+++ 2° Semestre/Desenvolvimento Orientado a Objetos/AtividadePython3.py
@@ -1,38 +1,98 @@
 from abc import ABC, abstractmethod
 import re
 
+# Classe abstrata
 class Evento(ABC):
-  def __init__(self, nome, data, local):
-    self.nome = nome
-    self.data = data
-    self.local = local
+    def __init__(self, nome, data, local):
+        self.nome = nome
+        self.data = data
+        self.local = local
 
-    # Método abstrato
+        # Método abstrato
+        def detalhes(self):
+            pass
+
+        @nome.setter
+        def nome(self, nome):
+            self.nome = nome
+
+        @property
+        def nome(self):
+            return self.nome
+
+        @data.setter
+        def data(self, data):
+            if not re.match(r'\d{2}/\d{2}/\d{4}', data):
+                raise ValueError("Data deve estar no formato DD/MM/AAAA.")
+            self.data = data
+
+        @property
+        def data(self):
+            return self.data
+
+        @local.setter
+        def local(self, local):
+            self.local = local
+
+        @property
+        def local(self):
+            return self.local
+
+
+# Subclasse Congresso
+class Congresso(Evento):
+    def __init__(self, nome, data, local, n_trabalhos):
+        super().__init__(nome, data, local)
+        self.n_trabalhos = n_trabalhos
+
+    @n_trabalhos.setter
+    def n_trabalhos(self, n_trabalhos):
+        self.n_trabalhos = n_trabalhos
+
+    @property
+    def n_trabalhos(self):
+        return self.n_trabalhos
+
     def detalhes(self):
-      pass
+        return f"Nome: {self.nome}\nData: {self.data}\nLocal: {self.local}\nTabalhos: {self.n_trabalhos}"
 
-    @nome.setter
-    def nomeSetter(self, nome):
-      self.nome = nome
 
-    # getter
-    def nomeGetter(self):
-        return self._nome
+# Subclasse Palestra
+class Palestra(Evento):
+    def __init__(self, nome, data, local, palestrante):
+        super().__init__(nome, data, local)
+        self.palestrante = palestrante
 
-    @data.setter
-    def data(self, data):
-        if not re.match(r'\d{2}/\d{2}/\d{4}', data):
-            raise ValueError("Data deve estar no formato DD/MM/AAAA.")
-        self._data = data
+    @palestrante.setter
+    def palestrante(self, palestrante):
+        self.palestrante = palestrante
 
-    # getter
-    def dataGetter(self):
-        return self.data
+    @property
+    def palestrante(self):
+        return self.palestrante
 
-    @local.setter
-    def localSetter(self, local):
-      self.local = local
+    def detalhes(self):
+        return f"Nome: {self.nome}\nData: {self.data}\nLocal: {self.local}\nPalestrante: {self.palestrante}"
 
-    # getter
-    def localGetter(self):
-        return self.local
+
+# Subclasse Workshop
+class Palestra(Evento):
+    def __init__(self, nome, data, local, vagas):
+        super().__init__(nome, data, local)
+        self.vagas = vagas
+
+    @vagas.setter
+    def vagas(self, vagas):
+        self.vagas = vagas
+
+    @property
+    def vagas(self):
+        return self.vagas
+
+    def detalhes(self):
+        return f"Nome: {self.nome}\nData: {self.data}\nLocal: {self.local}\nVagas: {self.vagas}"
+
+
+def mostrarDetalhesDoEvento(evento):
+    print(evento.detalhes())
+
