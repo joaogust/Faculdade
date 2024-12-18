2ee798598ef4607460b209c60c2603eb9d3a1f3c
João Gustavo <joaogustavodossantoss@gmail.com>
2024-12-18 16:28:39 -0300

    Incluindo o Mergesort

--- 2° Semestre/Estrutura de Dados/TrabalhoEDD.c
+++ 2° Semestre/Estrutura de Dados/TrabalhoEDD.c
@@ -22,6 +22,18 @@ void bubbleSort(int *vetor, int t){
 	}
 }
 
+void insertionSort(int *vetor, int t) {
+	int i=0, j, x;
+	
+	for (i=1; i<=t; i++) {
+		x = vetor[i];
+		for (j=i; j>0 && x<vetor[j-1]; j--) {
+			vetor[j] = vetor[j-1];
+		}
+		vetor[j] = x;
+	}
+}
+
 void selectionSort(int *vetor, int t){
 	int i, j, min, x;
 	
@@ -33,6 +45,55 @@ void selectionSort(int *vetor, int t){
 		x = vetor[min];
 		vetor[min] = vetor[i];
 		vetor[i] = x;
+	}
+}
+	
+void conquista(int *vetor, int esq, int dir, int meio) {
+	int tamEsq = meio - esq + 1;					
+	int tamDir = dir - meio;						
+	int auxEsq[tamEsq], auxDir[tamDir];
+	
+	for (int i = 0; i < tamEsq; i++) {
+        auxEsq[i] = vetor[i + esq];
+	}
+    for (int j = 0; j < tamDir; j++) {
+        auxDir[j] = vetor[j + meio + 1];
+	}
+	
+	int i = 0, j = 0, k = esq;
+	
+	while(i < tamEsq && j < tamDir) {
+		if(auxEsq[i] < auxDir[j]) {	
+			vetor[k] = auxEsq[i];
+			i++;
+		}
+		else {
+			vetor[k] = auxDir[j];
+			j++;
+		}
+		k++;
+	}
+	
+    while (i < tamEsq) {
+        vetor[k] = auxEsq[i];
+        i++;
+        k++;
+    }
+
+    while (j < tamDir) {
+        vetor[k] = auxDir[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int *vetor, int esq, int dir) {  
+	if (esq < dir) {			                    
+		int meio = (esq + dir) / 2;
+	
+	mergeSort(vetor, esq, meio);
+	mergeSort(vetor, meio + 1, dir);
+	conquista(vetor, esq, dir, meio);				
 	}
 }
 
@@ -69,7 +130,6 @@ int main() {
 		return 1;
 	} 
 	
-	
 	printf("\n\n--------------- Bubblesort ---------------\n\n");
 	
 	for(t = TAM; t <= 5000; t += TAM) {
@@ -84,6 +144,31 @@ int main() {
 				
 				clock_t begin = clock();
 				bubbleSort(vetor, t);
+				clock_t end = clock();
+				
+				time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+				
+			}
+		printf("%d: %f\n", t, time_spent/1000);
+		time_spent = 0;
+		
+		free(vetor);
+	}
+	
+	printf("\n\n--------------- Insertionsort ---------------\n\n");
+	
+	for(t = TAM; t <= 5000; t += TAM) {
+		vetor = (int *) malloc (t * sizeof(int)); 
+		if(vetor == NULL) {
+			printf("Nao foi possível alocar memória para o vetor...");
+			fclose(file);
+			return 1;
+		}
+			for(int f=0; f<1000; f++) {
+				leituraArq(vetor, file, t);
+				
+				clock_t begin = clock();
+				insertionSort(vetor, t);
 				clock_t end = clock();
 				
 				time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
@@ -118,7 +203,32 @@ int main() {
 		time_spent = 0;
 		
 		free(vetor);
-	}
+	} 
+		
+	printf("\n\n--------------- Mergesort ---------------\n\n");
+	
+	for(t = TAM; t <= 5000; t += TAM) {
+		vetor = (int *) malloc (t * sizeof(int)); 
+		if(vetor == NULL) {
+			printf("Nao foi possível alocar memória para o vetor...");
+			fclose(file);
+			return 1;
+		}
+			for(int f=0; f<1000; f++) {
+				leituraArq(vetor, file, t);
+				
+				clock_t begin = clock();
+				mergeSort(vetor, 0 , t-1);
+				clock_t end = clock();
+				
+				time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+				
+			}
+		printf("%d: %f\n", t, time_spent/1000);
+		time_spent = 0;
+		
+		free(vetor);
+	} 
 		
 	fclose(file);
 }
