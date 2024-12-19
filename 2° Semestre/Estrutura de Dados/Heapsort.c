ebcb118f89ae969ad4d26be84bc46d1e53f21280
João Gustavo <joaogustavodossantoss@gmail.com>
2024-12-19 12:17:09 -0300

    Melhoria no código do Heapsort ainda incompleto

--- 2° Semestre/Estrutura de Dados/Heapsort.c
+++ 2° Semestre/Estrutura de Dados/Heapsort.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 #define TAM 8
 
-void heapSort(int *vetor) {
-	int i = 0;
-	
-	sift(vetor, i)
-}
-
-void sift(int *vetor, int i){
-	int esq = 2*i;
-	int dir = 2*i+1;
-	if (esq <= TAM && v[esq] > v[i])
-		int maior = esq;
-	else
+void sift(int *vetor, int i, int t){
+	int esq = 2 * i;	
+	int dir = 2 * i + 1;
+	int maior;
+	if (esq <= t && vetor[esq] > vetor[i]) {
+		maior = esq;
+	} else {
 		maior = i;
-	if (dir <= TAM && v[dir] > v[maior])
+	}
+	if (dir <= t && vetor[dir] > vetor[maior]) {
 		maior = dir;
+	}
 	if (maior != i) {
-		aux = v[i];
-		v[i] = v[maior];
-		v[maior] = aux;
-		sift(maior, TAM);
+		int aux = vetor[i];
+		vetor[i] = vetor[maior];
+		vetor[maior] = aux;
+		sift(vetor, maior, t);
 	}
 }
 
+void build(int *vetor, int i, int t) {
+	for (i = t/2; i>0; i--) {
+		sift(vetor, i, t);
+	}
+}
+
+void heapSort(int *vetor, int t) {
+	int i = 0;
+	build(vetor, i, t);
+	for (i = t; i>0; i--) {
+		int aux = vetor[i];
+		vetor[i] = vetor[0];
+		vetor[0] = aux;
+		sift(vetor, 0, i-1);
+	}
+}
 	
-	int main () {
+int main () {
 	int vetor[TAM] = {8, 7, 6, 5, 4, 3, 2, 1};
-	
-	heapSort(vetor);
+	int t = TAM;
+	heapSort(vetor, t);
 	
 	for(int i = 0; i < TAM; i++) {
 		printf("%d ", vetor[i]);
